02cee54dc53492479779f96c1b742d7152616a8c
dhaura <dhaurapathirana@gmail.com>
2024-02-02 14:50:13 +0530

    Add h2-engine 2.2.224.wso2v2

---
+++ h2-engine/2.2.224.wso2v2/src/main/java/org/wso2/carbon/h2/osgi/H2DatabaseServiceActivator.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.h2.osgi;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.wso2.carbon.h2.osgi.database.H2DatabaseManager;
+
+/**
+ * This class is used to activate and deactivate the H2 database service.
+ */
+public class H2DatabaseServiceActivator implements BundleActivator {
+
+    /**
+     * This method is used to start the H2 database service.
+     *
+     * @param arg0 BundleContext.
+     */
+    public void start(BundleContext arg0) {
+
+        H2DatabaseManager.getInstance().initialize();
+    }
+
+    /**
+     * This method is used to stop the H2 database service.
+     *
+     * @param arg0 BundleContext.
+     */
+    public void stop(BundleContext arg0) {
+
+        H2DatabaseManager.getInstance().terminate();
+    }
+}
